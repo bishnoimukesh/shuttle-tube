37015c16c43df240ec826bbcaa8aab8761fb80e4
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2022-05-15 22:55:54 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);

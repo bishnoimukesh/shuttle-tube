a6eefb05285679cfb9a7fa97c73252fa0b1a5b5a
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2022-06-02 23:46:21 +0530

    feat: added remove all from history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,12 +1,13 @@
-import {Grid, GridItem, Box} from "@chakra-ui/react"
+import {Grid, GridItem, Box, Button} from "@chakra-ui/react"
 import {Card, Sidebar, Navbar} from '../components'
-// import {useAuthContext} from '../context/authContext'
+import {useAuthContext} from '../context/authContext'
 import {useVideo} from '../context/videoContext'
-// import { removeAllVideoFromHistory, removeVideoFromHistory } from '../utilities/videosFunction';
+import { removeAllVideoFromHistory, removeVideoFromHistory } from '../utilities/httpsHelper';
+import {AiFillDelete} from 'react-icons/ai'
 
 const History = () => {
-    // const {authState:{token}} = useAuthContext();
-    const {VideoState:{history}} = useVideo();
+    const {authState:{token}} = useAuthContext();
+    const {VideoState:{history}, VideoDispatch} = useVideo();
     console.log(history);
     return (
         <Grid h='200px' templateRows='repeat(2, 1fr)'
@@ -18,9 +19,16 @@ const History = () => {
                         <Navbar/>
                 </GridItem>
                 <GridItem colSpan={4} >
-                    <Box ml={'4'} mr={'4'} pb={'8'} >
+                        <Button onClick={()=>removeAllVideoFromHistory(VideoDispatch, token)}>Clear History</Button>
+                    <Box ml={'4'} mr={'4'} pb={'8'} display={'flex'} flexWrap={'wrap'} >
                         {history?.map((video) => (
+                            <Box position={'relative'}>
                             <Card cardData={video} key={video._id} />
+                            <Button position={'absolute'} bg={'red'} top={'0'} borderRadius={'10'} p={'2'} 
+                            id={video._id} onClick={(e)=>removeVideoFromHistory(e.target.id,VideoDispatch,token)}>
+                                <AiFillDelete size={'1.5rem'}/>
+                            </Button>
+                        </Box> 
                         ))}
                     </Box>
                 </GridItem>
